aadb40e4da8794e8340e13fa144c0c2a18f48aef
HodaeSsi <kfdream123@gmail.com>
2022-03-04 16:56:18 +0900

    빈 자동등록 탐색 위치와 기본 스캔 대상

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -6,6 +6,11 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+//1.        basePackages = "hello.core.member",
+        //스캔 시작 위치 및 탐색 범위(하위 패키지) 지정
+        //스캔 시간 단축 효과
+//2.        basePackageClasses = AutoAppConfig.class,
+//3.        default 옵션
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
